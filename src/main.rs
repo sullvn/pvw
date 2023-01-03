4a2e7755f89634cd2ae957cf189e04387b3bf35f
Kevin Sullivan <kevin@sull.vn>
2023-01-03 15:17:28 -0500

    Detect TTY stdin

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
-fn main() {
-    println!("Hello, world!");
+// use nix::sys::termios;
+use nix::unistd::isatty;
+use std::io::stdin;
+use std::os::fd::AsRawFd;
+
+fn main() -> std::io::Result<()> {
+    let fd = stdin().as_raw_fd();
+    let is_tty = isatty(fd)?;
+    if is_tty {
+        println!("Is tty");
+    } else {
+        println!("Is not tty");
+    }
+
+    Ok(())
 }
