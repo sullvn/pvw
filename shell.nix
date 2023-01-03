11e50acb9aa4497f8e60b5ca665538b549110ddd
Kevin Sullivan <kevin@sull.vn>
2023-01-03 14:12:49 -0500

    nix and rust-analyzer autocompletions

--- shell.nix
+++ shell.nix
@@ -5,7 +5,9 @@ in
   with nixpkgs;
   mkShell {
     buildInputs = [
-      nixpkgs.latest.rustChannels.stable.rust
+      (nixpkgs.latest.rustChannels.stable.rust.override {
+        extensions = ["rust-src"];
+      })
       nixpkgs.rust-analyzer
       nixpkgs.libiconv
       nixpkgs.llvmPackages.lldb
